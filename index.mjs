98560cb734183f45dd7bce5a5e4d837579540b5c
lim-jiahao <lim.jiahaoo@gmail.com>
2022-02-16 22:10:38 +0800

    set up .env

--- index.mjs
+++ index.mjs
@@ -1,9 +1,13 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import bindRoutes from './routers/index.mjs';
 
+dotenv.config();
+
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
+const PORT = process.env.PORT || 3004;
 
 const app = express();
 app.use(cookieParser());
@@ -17,5 +21,4 @@ app.use(cors({
 
 bindRoutes(app);
 
-const PORT = process.env.PORT || 3004;
 app.listen(PORT, () => console.log('Listening on port 3004'));
