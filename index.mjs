fdea9d212ef0e364871d4f19570a029444ec7018
lim-jiahao <lim.jiahaoo@gmail.com>
2022-02-16 09:48:40 +0800

    remove bloat

--- index.mjs
+++ index.mjs
@@ -1,25 +1,21 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import methodOverride from 'method-override';
-
+import cors from 'cors';
 import bindRoutes from './routes.mjs';
 
-// Initialise Express instance
-const app = express();
-// Set the Express view engine to expect EJS templates
-app.set('view engine', 'ejs');
-// Bind cookie parser middleware to parse cookies in requests
-app.use(cookieParser());
-// Bind Express middleware to parse request bodies for POST requests
-app.use(express.urlencoded({ extended: false }));
-// Bind method override middleware to parse PUT and DELETE requests sent as POST requests
-app.use(methodOverride('_method'));
-// Expose the files stored in the public folder
-app.use(express.static('public'));
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 
-// Bind route definitions to the Express application
+const app = express();
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cors({
+  credentials: true,
+  origin: FRONTEND_URL,
+}));
+
 bindRoutes(app);
 
-// Set Express to listen on the given port
 const PORT = process.env.PORT || 3004;
-app.listen(PORT);
+app.listen(PORT, () => console.log('Listening on port 3004'));
