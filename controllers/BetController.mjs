68b8a46199546d780341f50491e39c57f69a5cc1
lim-jiahao <lim.jiahaoo@gmail.com>
2022-02-18 00:25:50 +0800

    add post method to save bet

--- controllers/BetController.mjs
+++ controllers/BetController.mjs
@@ -2,14 +2,15 @@ import axios from 'axios';
 import BaseController from './BaseController.mjs';
 
 export default class BetController extends BaseController {
+  // eslint-disable-next-line class-methods-use-this
   async check(req, res) {
     const { numbersChecked } = req.body;
     const numbers = numbersChecked.reduce((acc, cur, i) => {
-      if (!cur) return acc;
       if (cur) {
         if (i + 1 < 10) return `${acc},0${i + 1}`;
         return `${acc},${i + 1}`;
       }
+      return acc;
     },
     '').slice(1);
 
@@ -20,9 +21,47 @@ export default class BetController extends BaseController {
       partsPurchased: '1',
       totalNumberOfParts: '1',
     };
+    try {
+      const results = await axios.post('https://www.singaporepools.com.sg/_layouts/15/TotoApplication/TotoCommonPage.aspx/CalculatePrizeForTOTO', data);
+      const info = JSON.parse(results.data.d);
 
-    const results = await axios.post('https://www.singaporepools.com.sg/_layouts/15/TotoApplication/TotoCommonPage.aspx/CalculatePrizeForTOTO', data);
-    results.data.numbers = numbers;
-    res.send(results.data);
+      const respData = {
+        winningNumbers: info.WinningNumbers.join(','),
+        prize: info.Prizes.length > 0 ? Number(info.Prizes[0].Total) : 0,
+        additionalNumber: info.AdditionalNumber,
+        numbers,
+      };
+      res.send(respData);
+    } catch (error) {
+      res.status(503).send({ error });
+    }
+  }
+
+  async save(req, res) {
+    const { numbers, prize } = req.body;
+    const { id } = req.userInfo;
+    try {
+      const ticket = await this.db.Ticket.create({
+        filename: 'abc',
+        userId: id,
+      });
+
+      const [digitOne, digitTwo, digitThree, digitFour, digitFive, digitSix] = numbers.split(',');
+
+      const bet = await this.model.create({
+        digitOne,
+        digitTwo,
+        digitThree,
+        digitFour,
+        digitFive,
+        digitSix,
+        profit: prize,
+        ticketId: ticket.id,
+      });
+
+      res.json({ bet });
+    } catch (error) {
+      res.status(503).send({ error });
+    }
   }
 }
--- routers/betRouter.mjs
+++ routers/betRouter.mjs
@@ -1,10 +1,12 @@
 import express from 'express';
 import BetController from '../controllers/BetController.mjs';
 import db from '../models/index.mjs';
+import checkAuth from '../middleware/auth.mjs';
 
 const router = express.Router();
 const controller = new BetController(db.Bet, db);
 
 router.post('/check', controller.check.bind(controller));
+router.post('/save', checkAuth, controller.save.bind(controller));
 
 export default router;
