6a6125322a83371bad07f8ace05b11f623b9a001
lim-jiahao <lim.jiahaoo@gmail.com>
2022-02-24 03:11:52 +0800

    integrate google cloud vision api

--- routers/index.mjs
+++ routers/index.mjs
@@ -1,11 +1,13 @@
 import userRouter from './userRouter.mjs';
 import betRouter from './betRouter.mjs';
 import ticketRouter from './ticketRouter.mjs';
+import visionRouter from './visionRouter.mjs';
 
 const bindRoutes = (app) => {
   app.use('/user', userRouter);
   app.use('/bet', betRouter);
   app.use('/ticket', ticketRouter);
+  app.use('/vision', visionRouter);
 };
 
 export default bindRoutes;
